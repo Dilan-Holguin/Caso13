4e9460273fce5d73e64c405362d94c234cb20a20
Dilan-Holguin <dilan.holguin@udea.edu.co>
2026-04-02 15:37:41 -0500

    feat: initial Spring Boot project setup

---
+++ src/main/java/com/eap08/domesticas/DomesticasApplication.java
@@ -0,0 +1,13 @@
+package com.eap08.domesticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomesticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomesticasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eap08/domesticas/DomesticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eap08.domesticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomesticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
