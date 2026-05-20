cd9e0c6ab679cf956a0738de623fbb55e4478a4d
Thomas Ciro <thomyciro@gmail.com>
2026-05-20 17:32:32 -0500

    some unit test added

--- src/test/java/com/eap08/domesticas/service/TareaServiceTest.java
+++ src/test/java/com/eap08/domesticas/service/TareaServiceTest.java
@@ -17,6 +17,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
+import java.time.LocalDate;
+
+import static org.mockito.Mockito.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -42,6 +45,33 @@ class TareaServiceTest {
 
     @InjectMocks
     private TareaService tareaService;
+
+    //funciones utiles para las pruebas
+    private Usuario crearUsuario(String email, Long id) {
+        Usuario usuario = new Usuario();
+        usuario.setUsuarioId(id);
+        usuario.setEmail(email);
+        return usuario;
+    }
+
+    private Hogar crearHogar(Long id) {
+        return Hogar.builder()
+                .hogarId(id)
+                .nombre("Hogar Test")
+                .build();
+    }
+
+    private Tarea crearTarea(Long tareaId, Hogar hogar) {
+        return Tarea.builder()
+                .tareaId(tareaId)
+                .hogar(hogar)
+                .titulo("Lavar la loza")
+                .descripcion("Descripción")
+                .categoria("Cocina")
+                .estado(Tarea.ESTADO_PENDIENTE)
+                .build();
+    }
+
 
     @Test
     void shouldCreateTaskSuccessfully() {
@@ -137,4 +167,167 @@ class TareaServiceTest {
 
         verify(tareaRepo, never()).save(any(Tarea.class));
     }
+
+    //CASO FELIZ
+
+    //CP 18 edición exitosa
+    @Test
+    void shouldUpdateTaskSuccessfully() {
+
+        // Arrange
+        Long tareaId = 1L;
+        Long hogarId = 1L;
+        String email = "ana@example.com";
+
+        Usuario usuario = crearUsuario(email, 10L);
+        Hogar hogar = crearHogar(hogarId);
+        Tarea tarea = crearTarea(tareaId, hogar);
+
+        TareaRequest.UpdateTareaRequest request =
+                new TareaRequest.UpdateTareaRequest(
+                        "Lavar la loza y secarla",
+                        null,
+                        "Cocina",
+                        null,
+                        null
+                );
+
+        when(tareaRepo.findById(tareaId)).thenReturn(Optional.of(tarea));
+        when(usuarioRepo.findByEmail(email)).thenReturn(Optional.of(usuario));
+        when(usuarioHogarRepo.existsByIdUsuarioIdAndIdHogarId(10L, hogarId)).thenReturn(true);
+        when(tareaRepo.save(any(Tarea.class))).thenAnswer(inv -> inv.getArgument(0));
+
+        // Act
+        TareaResponse.TareaData result = tareaService.actualizarTarea(tareaId, request, email);
+
+        // Assert
+        assertThat(result.titulo()).isEqualTo("Lavar la loza y secarla");
+        assertThat(result.categoria()).isEqualTo("Cocina");
+    }
+
+    //CASOS EXCEPCIONALES
+    //CP 20 tarea inexistente
+    @Test
+    void shouldThrowWhenTaskDoesNotExist() {
+        // Arrange
+        Long tareaId = 99999L;
+
+        TareaRequest.UpdateTareaRequest request =
+                new TareaRequest.UpdateTareaRequest("Nuevo titulo", null, "Cocina", null, null);
+
+        when(tareaRepo.findById(tareaId)).thenReturn(Optional.empty());
+
+        // Act + Assert
+        assertThatThrownBy(() -> tareaService.actualizarTarea(tareaId, request, "ana@example.com"))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("Tarea no encontrada");
+    }
+
+    //CP 21 usuario sin permisos
+    @Test
+    void shouldThrowWhenUserDoesNotBelongToHousehold() {
+        // Arrange
+        Long tareaId = 1L;
+        Long hogarId = 1L;
+
+        String email = "externo@example.com";
+        Usuario usuario = crearUsuario(email, 20L);
+        Hogar hogar = crearHogar(hogarId);
+        Tarea tarea = crearTarea(tareaId, hogar);
+
+        TareaRequest.UpdateTareaRequest request = new TareaRequest.UpdateTareaRequest("Nuevo titulo", null, "Cocina", null, null);
+
+        when(tareaRepo.findById(tareaId)).thenReturn(Optional.of(tarea));
+        when(usuarioRepo.findByEmail(email)).thenReturn(Optional.of(usuario));
+        when(usuarioHogarRepo.existsByIdUsuarioIdAndIdHogarId(20L, hogarId)).thenReturn(false);
+
+        // Act + Assert
+        assertThatThrownBy(() -> tareaService.actualizarTarea(tareaId, request, email))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("No perteneces a este hogar");
+    }
+    //CP 22 cambiar estado a En_progreso
+
+    @Test
+    void shouldUpdateTaskStatusToEnProgreso() {
+
+        // Arrange
+        Long tareaId = 1L;
+        Long hogarId = 1L;
+
+        String email = "ana@example.com";
+
+        Usuario usuario = crearUsuario(email, 10L);
+        Hogar hogar = crearHogar(hogarId);
+        Tarea tarea = crearTarea(tareaId, hogar);
+
+        TareaRequest.UpdateStatusRequest request = new TareaRequest.UpdateStatusRequest("En_progreso");
+
+        when(tareaRepo.findById(tareaId)).thenReturn(Optional.of(tarea));
+        when(usuarioRepo.findByEmail(email)).thenReturn(Optional.of(usuario));
+        when(usuarioHogarRepo.existsByIdUsuarioIdAndIdHogarId(10L, hogarId)).thenReturn(true);
+        when(tareaRepo.save(any(Tarea.class))).thenAnswer(inv -> inv.getArgument(0));
+
+        // Act
+        TareaResponse.TareaData result = tareaService.actualizarEstado(tareaId, request, email);
+
+        // Assert
+        assertThat(result.estado()).isEqualTo("En_progreso");
+    }
+
+    //CP 23 cambiar a Completada
+
+    @Test
+    void shouldUpdateTaskStatusToCompleted() {
+
+        // Arrange
+        Long tareaId = 1L;
+        Long hogarId = 1L;
+
+        String email = "ana@example.com";
+
+        Usuario usuario = crearUsuario(email, 10L);
+        Hogar hogar = crearHogar(hogarId);
+        Tarea tarea = crearTarea(tareaId, hogar);
+
+        TareaRequest.UpdateStatusRequest request = new TareaRequest.UpdateStatusRequest("Completada");
+
+        when(tareaRepo.findById(tareaId)).thenReturn(Optional.of(tarea));
+        when(usuarioRepo.findByEmail(email)).thenReturn(Optional.of(usuario));
+        when(usuarioHogarRepo.existsByIdUsuarioIdAndIdHogarId(10L, hogarId)).thenReturn(true);
+        when(tareaRepo.save(any(Tarea.class))).thenAnswer(inv -> inv.getArgument(0));
+
+        // Act
+        TareaResponse.TareaData result = tareaService.actualizarEstado(tareaId, request, email);
+
+        // Assert
+        assertThat(result.estado()).isEqualTo("Completada");
+    }
+
+    // CP 24 estado inválido
+    @Test
+    void shouldThrowWhenStatusIsInvalid() {
+
+        // Arrange
+        Long tareaId = 1L;
+        Long hogarId = 1L;
+
+        String email = "ana@example.com";
+
+        Usuario usuario = crearUsuario(email, 10L);
+        Hogar hogar = crearHogar(hogarId);
+        Tarea tarea = crearTarea(tareaId, hogar);
+
+        TareaRequest.UpdateStatusRequest request = new TareaRequest.UpdateStatusRequest("Cancelada");
+
+        when(tareaRepo.findById(tareaId)).thenReturn(Optional.of(tarea));
+        when(usuarioRepo.findByEmail(email)).thenReturn(Optional.of(usuario));
+        when(usuarioHogarRepo.existsByIdUsuarioIdAndIdHogarId(10L, hogarId)).thenReturn(true);
+
+        // Act + Assert
+        assertThatThrownBy(() -> tareaService.actualizarEstado(tareaId, request, email))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("Estado no valido");
+    }
 }
+
